844f6f4f651581953efdec53ea164513aa8aaad6
Ryan Mendieta <ryan.mendieta@gmail.com>
2013-03-31 01:38:43 -0400

    now submits form data to db. very sloppy/insecure! todo: form validation (make sure all fields filled in) print out current records put in link to map page protect the script from outside access (move to different dir) make sure data types are OK for reading into map application

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-from pygeocoder import Geocoder
+#!/usr/bin/env python2.7
+
 import psycopg2
+from pygeocoder import Geocoder
+import cgi
+import cgitb; cgitb.enable()
 
-
-con = psycopg2.connect(host = '127.0.0.1', database='foodtruck',user='',password='')
+form = cgi.FieldStorage()
+print "Content-type: text/html"
+print
+print "Hello world"
+con = psycopg2.connect(host = '127.0.0.1', database='',user='',password='')
 cur = con.cursor()
 
-#cur.execute("""select * from trucks;""")
-#result = cur.fetchall()
-
+cur.execute("""select * from trucks2;""")
+result = cur.fetchall()
+print result
 
 #try:
 #    cur.execute("""create table trucks2 (ID serial primary key, Truck VarChar(30), Location VarChar(100), X decimal, Y decimal, Arrive VarChar(4), Depart VarChar(4), Date VarChar(6));""")
@@ -15,21 +22,33 @@ cur = con.cursor()
 #    print e
 
 
-def ReadForm(truck, loc, arr, dep, date):
-    geo = Geocoder.geocode(loc)
-    x = geo.coordinates[1]
-    y = geo.coordinates[0]
-    sql = "insert into trucks2 (Truck,Location,X,Y,Arrive,Depart,Date) values ('" + truck + "','" + loc + "'," + str(x) + "," + str(y) + ",'" + arr + "','" + dep + "','" + date + "');"
-    cur.execute(sql)
+def updateDB(truck, loc, arr, dep, date):
+	geo = Geocoder.geocode(loc)
+	x = geo.coordinates[1]
+	y = geo.coordinates[0]
+	
+    #sql = "insert into trucks2 (Truck,Location,X,Y,Arrive,Depart,Date) values ('" + truck + "','" + loc + "'," + str(x) + "," + str(y) + ",'" + arr + "','" + dep + "','" + date + "');"
+	#print sql
+    
+	cur.execute("""INSERT INTO trucks2 (Truck, Location,X,Y,Arrive,Depart,Date) VALUES (%s,%s,%s,%s,%s,%s,%s)""", (truck, loc, str(x), str(y), arr, dep, date))
+	con.commit()
+	print 'shit was saved to db.'
 
-form_truck = "TruckyTrucky2"
-form_loc = "30 Elmwood Ave Buffalo NY"
-form_arr = "1030"
-form_dep = "1330"
-form_date = "030812"
+form_truck = form["form_truck"].value
+form_loc = form["form_loc"].value
+form_arr = form["form_arrive"].value
+form_dep = form["form_depart"].value
+form_date = form["form_date"].value
+print form_truck, form_loc, form_arr, form_dep, form_date, '<br/><br/>'
 
-ReadForm(form_truck,form_loc,form_arr,form_dep,form_date)
+updateDB(form_truck,form_loc,form_arr,form_dep,form_date)
 
-con.commit()
+#cur.execute("""select * from trucks2;""")
+#result = cur.fetchall()
+
+
+#print "Data submitted to trucks2 for truck: ", form_truck
+#print "<br/>Stuff in db:<br/>"
+#print result
+
 con.close()
-
